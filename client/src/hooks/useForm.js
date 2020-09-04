3d137403ea65e63bd56e5c872b9d78eeb707246f
Tom Bauer <tbauer1898@gmail.com>
2020-09-04 11:46:53 -0500

    Done with MVP.

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,21 @@
 // write your custom hook here to control your checkout form
+import React from 'react';
+import useLocalStorage from './useLocalStorage'
+
+const useForm = (key, initialValue) => {
+
+    const [values, setValues] = useLocalStorage(key, initialValue);
+
+    const handleChanges = e => {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        });
+
+    }
+
+    return [values, handleChanges]
+
+};
+
+export default useForm;
